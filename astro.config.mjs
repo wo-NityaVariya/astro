229afc51dba0fd3959dbd8586d53e1745266ce84
astrops <nityav.webosmotic@gmail.com>
2025-04-09 10:40:30 +0530

    Initial Astro commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+
+// https://astro.build/config
+export default defineConfig({
+  output : "static",
+
+  vite:{
+    plugins: [
+      tailwindcss()
+    ]
+  }
+});
